319c7405ac37cc4456fccd4008888eab070c1f40
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-01-22 22:33:31 -0500

    style: apply black to all files in the project directory

--- src/Convert_lines.py
+++ src/Convert_lines.py
@@ -1,21 +1,22 @@
-'''
+"""
 
 本文使用的是修饰后的广度优先搜索算法，每次遇到分支会产生一次新的搜索，目的是减少晶粒生成的骨架中的不同直线的数量，便于后续的霍夫转换分析。
 
 建议先阅读广度优先搜索算法，以及霍夫转换相关资料。
 
-'''
+"""
 
 import copy
 from skimage import morphology
 import numpy as np
 
-class LineBud():
+
+class LineBud:
     def __init__(self, i, j):
-        self.start_position = (i,j)
+        self.start_position = (i, j)
         self.current_position = (i, j)
         self.memory = []  # recording history of steps
-        self.status = 'open'  # 'open' if (the LineBud can still grow) else 'close'
+        self.status = "open"  # 'open' if (the LineBud can still grow) else 'close'
 
     def go(self, i=0, j=0):
         self.memory.append(list(self.current_position))
@@ -26,8 +27,10 @@ class LineBud():
         self.memory.append(list(self.current_position))
         self.current_position = (x, y)
 
+
 def SearchLines(skeleton):
     skeleton = copy.deepcopy(skeleton)
+
     def Look_around(linebud, skeleton):
         i, j = linebud.current_position
         m, n = skeleton.shape
@@ -49,9 +52,13 @@ def SearchLines(skeleton):
 
         # find directions to search along the skeleton
         coordinates = [[i, j] for i in rows for j in cols]
-        coordinates.remove(list(linebud.current_position))  # current position is not recorded
+        coordinates.remove(
+            list(linebud.current_position)
+        )  # current position is not recorded
         coordinates = np.array(coordinates)
-        solid_points = np.argwhere(np.array([skeleton[i, j] for i, j in coordinates]) == 255)
+        solid_points = np.argwhere(
+            np.array([skeleton[i, j] for i, j in coordinates]) == 255
+        )
 
         # reorganize the dimension of the output arrays
         out = []
@@ -65,8 +72,18 @@ def SearchLines(skeleton):
     # Initialize
     solid_index = np.argwhere(skeleton == 255)
     corner = []
-    corner.extend([solid_index[i] for i in [func(solid_index[:, 1]) for func in [np.argmin, np.argmax]]])  # left and right
-    corner.extend([solid_index[i] for i in [func(solid_index[:, 0]) for func in [np.argmin, np.argmax]]])  # top and bottom
+    corner.extend(
+        [
+            solid_index[i]
+            for i in [func(solid_index[:, 1]) for func in [np.argmin, np.argmax]]
+        ]
+    )  # left and right
+    corner.extend(
+        [
+            solid_index[i]
+            for i in [func(solid_index[:, 0]) for func in [np.argmin, np.argmax]]
+        ]
+    )  # top and bottom
     # make sure the initial point is at either ends of the skeleton
     for i in range(len(corner)):
         start_point = corner[i]
@@ -87,15 +104,21 @@ def SearchLines(skeleton):
             break
         flag += 1
         for j in range(len(total_buds)):
-            current_position = total_buds[j].current_position  # save the value in case it is changed by updating
-            solid_coordinates = Look_around(total_buds[j], skeleton)  # solid pixels around the tip of current LineBud
+            current_position = total_buds[
+                j
+            ].current_position  # save the value in case it is changed by updating
+            solid_coordinates = Look_around(
+                total_buds[j], skeleton
+            )  # solid pixels around the tip of current LineBud
 
             if len(solid_coordinates) == 0:  # there are no solid pixels around
                 total_buds[j].memory.append(list(total_buds[j].current_position))
-                total_buds[j].status = 'close'
+                total_buds[j].status = "close"
             else:
                 for k in range(len(solid_coordinates)):
-                    if k == 0:  # searching along the first direction should not create new LineBud to bifurcate
+                    if (
+                        k == 0
+                    ):  # searching along the first direction should not create new LineBud to bifurcate
                         total_buds[j].moveto(*solid_coordinates[k])
                     else:
                         new_bud = LineBud(*current_position)  # bifurcate
@@ -103,13 +126,15 @@ def SearchLines(skeleton):
                         total_buds.append(new_bud)
                     skeleton[solid_coordinates[k][0], solid_coordinates[k][1]] = 0
 
-        lines.extend(list(filter(lambda x: x.status == 'close', total_buds)))  # lineBuds that stop grow
-        total_buds = list(filter(lambda x: x.status == 'open', total_buds))
+        lines.extend(
+            list(filter(lambda x: x.status == "close", total_buds))
+        )  # lineBuds that stop grow
+        total_buds = list(filter(lambda x: x.status == "open", total_buds))
     return lines
 
 
 def Skeletonize(bi_grain):
-    bi_grain = (bi_grain >= 125).astype('uint8')
+    bi_grain = (bi_grain >= 125).astype("uint8")
     skeleton = morphology.skeletonize(bi_grain) * 255
-    skeleton = skeleton.astype('uint8')
+    skeleton = skeleton.astype("uint8")
     return skeleton
